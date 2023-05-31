270db5a03592ada730ff27cee45f35c787df04ea
YiChunChenTW <10856039@ntub.edu.tw>
2023-06-01 01:50:08 +0800

    新增群組上傳題目

--- first/views.py
+++ first/views.py
@@ -19,13 +19,6 @@ def post_list(request):
 
     return render(request, "post_list.html", {"posts": posts})
 
-def post_openlist(request):
-    posts = Post.objects.prefetch_related("tags")
-    if "tag_id" in request.GET:
-        posts = posts.filter(tags__id=request.GET["tag_id"])
-
-    return render(request, "post_openlist.html", {"posts": posts})
-
 def post_grouplist(request):
     posts = Post.objects.prefetch_related("tags")
     if "tag_id" in request.GET:
@@ -44,7 +37,7 @@ def post_create(request):
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         form.save()
-        messages.success(request, "文章建立成功")
+        messages.success(request, "題目上傳成功")
         return redirect("post_list")
 
     return render(request, "post_create.html", {"form": form})
@@ -54,7 +47,7 @@ def post_groupcreate(request):
     if form.is_valid():
         form.save()
         messages.success(request, "群組建立成功")
-        return redirect("post_list")
+        return redirect("post_grouplist")
 
     return render(request, "post_groupcreate.html", {"form": form})
 
