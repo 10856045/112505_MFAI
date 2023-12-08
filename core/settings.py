627aabd3e0d47d2b5373f63109b3da77b7bca0d7
10856045 <10856045@ntub.edu.tw>
2023-12-08 12:28:43 +0800

    post-list介面修改

--- core/settings.py
+++ core/settings.py
@@ -138,6 +138,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
